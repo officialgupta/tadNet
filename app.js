c24088dc4819f7e148eb54635a553ac644bda521
Mayank Gupta <mguptamayankg@gmail.com>
2017-11-11 20:50:08 +0000

    removed STYLUS! and nice bg

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
 var synaptic = require('synaptic');
 
 var Neuron = synaptic.Neuron,
@@ -28,7 +27,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
