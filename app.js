960d1ce1522a397804a81b59ff9c9f36f2f708e7
Yu Jo <zoey5538@gmail.com>
2017-11-12 08:16:35 +0000

    Remove jQuery and chunkfive-fontface

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var synaptic = require('synaptic');
-var $ = require('jQuery');
 
 var Neuron = synaptic.Neuron,
 	Layer = synaptic.Layer,
